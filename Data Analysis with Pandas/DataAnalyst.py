d6685fc8b5e3fbafa21e9018c3579c3dca3427b0
Luis Ruben Gonzalez Pagan <gonzalez.engineer42@gmail.com>
2025-02-27 00:23:29 -0400

    Data Analysis with Panda
    File Path with python and Pandas for Data analysis

---
+++ Data Analysis with Pandas/DataAnalyst.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Provide the path to your Excel file
+excel_file_path = "C:/Users/gonza/OneDrive/Documents/Practica de charts.xlsx"
+
+try:
+    # Read the Excel file into a DataFrame
+    data = pd.read_excel(excel_file_path)
+
+    # Display the first few rows of the DataFrame
+    print("First few rows of the DataFrame:")
+    print(data.head())
+except FileNotFoundError:
+    print("The specified file was not found. Please check the file path.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
---
+++ Data Analysis with Pandas/Filecheck.py
@@ -0,0 +1,6 @@
+import os
+
+if os.path.isfile("C:/Users/gonza/OneDrive/Documents/Practica de charts.xlsx"):
+    print("File exists!")
+else:
+    print("File does not exist at the specified path.")
